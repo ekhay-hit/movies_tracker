d2ed9e8cda7bb0ecbaab6a87a8d7cd06f735800b
Houssine <ekhay1@outlook.com>
2024-10-23 20:11:37 -0400

    Build rating component and styling it using inline styles

---
+++ src/components/StartRating.js
@@ -0,0 +1,28 @@
+// setting the style for the rating
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "16px",
+};
+
+const startContainerStyle = {
+  display: "flex",
+  gap: "4px",
+};
+
+const textStyle = {
+  lineHeight: "1",
+  margin: "0",
+};
+export default function StartRating({ maxRating = 5 }) {
+  return (
+    <div style={containerStyle}>
+      <div style={startContainerStyle}>
+        {Array.from({ length: `${maxRating}` }, (_, i) => (
+          <span>S{i + 1}</span>
+        ))}
+      </div>
+      <p style={textStyle}>10</p>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StartRating from "./components/StartRating.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StartRating />
+    {/* <App /> */}
   </React.StrictMode>
 );
