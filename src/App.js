8473d8b1b54570ff34f4460e2e12d2b29a855769
Houssine <ekhay1@outlook.com>
2024-11-12 20:29:17 -0500

    added Loading state to showing the Loader component that indicate that the date is being loading and add the logic for the state and rendering

--- src/App.js
+++ src/App.js
@@ -53,16 +53,19 @@ const KEY = "1e75e89a";
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const query = "interstellar";
 
   // use useEffect to fetch data so it only fetch once
   useEffect(function () {
     async function fetchMovies() {
+      setIsLoading(true);
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
       );
       const data = await res.json();
       setMovies(data.Search);
+      setIsLoading(false);
     }
     fetchMovies();
   }, []);
@@ -77,9 +80,7 @@ export default function App() {
       </Nav>
 
       <Main>
-        <Box>
-          <MoviesList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MoviesList movies={movies} />}</Box>
         <Box>
           <WatchedSummary watched={watched} />
           <WatchedMoviesList watched={watched} />
@@ -87,6 +88,11 @@ export default function App() {
       </Main>
     </>
   );
+}
+
+// component use to display loading when data is being fetched
+function Loader() {
+  return <p className="loader">Loading...</p>;
 }
 
 // Navigation component
